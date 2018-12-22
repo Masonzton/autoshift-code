0caa072917d93aeb4b176d840c6b468038813d68
nick anselmo <nickanselmo912@gmail.com>
2018-12-22 18:51:51 -0500

    gui update for mac compatability

--- gui/tachometer.py
+++ gui/tachometer.py
@@ -1,4 +1,5 @@
 from tkinter import *
+#import tkinter
 #tkinter comes preinstalled on windows python,
 #not sure for other os
 import time
@@ -12,7 +13,8 @@ def y(r,yc=200): #yc is where tac is centered
     '''y position along circle'''
     return 100*sin((330/35*r+90)*pi/180)+yc
 #open serial port
-ser = serial.Serial('COM6',57600,timeout=0)
+#ser = serial.Serial(port='/dev/cu.DSDTECHHC-06-DevB',baudrate=9600,timeout=0)
+ser = serial.Serial(port='/dev/cu.usbmodem14101',baudrate=9600,timeout=0)
 print("connected to: " + ser.portstr)
 
 #start tkinter
@@ -34,7 +36,7 @@ class tachometer:
         c.create_oval(xc-radius,yc-radius,xc+radius,yc+radius)
         for i in range(0,40,5):#creates numbers along circle 5 multiples
             c.create_text(x(i,xc)+(x(i,xc)-xc)*0.2,y(i,yc)+(y(i,yc)-yc)*0.2,text=str(i),font='20')
-        c.create_rectangle(x(0,xc),y(35,yc),x(35,xc),y(0,yc)
+        c.create_rectangle(x(0,xc),y(35,yc),x(35,xc),y(0,yc),
         fill=bkgcolor,outline=bkgcolor)  #colors over last part of tac
     def update(self,rpm):
         c.delete(self.arrow)
@@ -45,13 +47,15 @@ class tachometer:
 tac1 = tachometer(200,300)
 
 while True:
+  time.sleep(.1)
   line = ser.readline()
   if line:
       #this code reads the rpm value
       string_line = str(line)
       a=-5 #-1 if using print; -5 if using println for serial port
-      rpm = int(string_line[2:a])
-      rpm = rpm/100 ##### scale by 100 later
+      if string_line[2:a]!='':
+          rpm = int(string_line[2:a])
+          rpm = rpm/100 ##### scale by 100 later
 
       #update rpm value
       #rpm value is meassred in 100's of rpm's from 0 to 35 hundred
