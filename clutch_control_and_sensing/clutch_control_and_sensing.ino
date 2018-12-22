2777692829cfd0f208f36445450c8bd1f8645dfb
NickA912 <46060664+NickA912@users.noreply.github.com>
2018-12-22 17:00:28 -0500

    Update clutch_control_and_sensing.ino

--- clutch_control_and_sensing/clutch_control_and_sensing.ino
+++ clutch_control_and_sensing/clutch_control_and_sensing.ino
@@ -1,11 +1,18 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
 #define DIR 6
 #define PWM 5
 #define POS A6
-#define CIN 250
-#define COUT 100
+#define CIN 290
+#define COUT 120
 #define NUM_PULSE 12
-#define NUM_PULSE2 8
+#define NUM_PULSE2 12
+#define TX 8
+#define RX 9
+#define BT_POWER 10
 const int changeRPM[] = {2200,2300,2400,2600};
+const int changeUpRPM[] = {3500, 3500, 3500, 3500};
 
 int data[8];
 long prevTime=0;
@@ -23,6 +30,9 @@ int failedResponseCounter=0;
 bool doNeutral = false;
 int nwatchdog =0;
 int wait_send_get_gear = 0;
+bool isStopped=true;
+
+SoftwareSerial btSerial = SoftwareSerial(RX, TX);
 
 
 //functions
@@ -39,26 +49,45 @@ void NewinterruptIRQ() {
   long currentTime=micros();
   if (prevTime2<currentTime) {
     rpm2=60*1000000/((currentTime-prevTime2)*NUM_PULSE2);
+    //Serial.println("start");
+    //Serial.println(rpm2);
     data[count] = rpm2;
     count++;
     if (count>7){
       count = 0;
     }
-    rpm2=average(data);
+    rpm2=average(data, sizeof(data));
+    for (int i=0; i<8; i++) {
+      //Serial.print(i);
+      //Serial.print(": ");
+      //Serial.println(data[i]);
+    }
+    //Serial.println(rpm2);
   }
   prevTime2=currentTime;
   updateflag2 = true;
 }
-int average( int a[]) {
+
+int average( int a[], int len) {
   long sum = 0;
-  int len = sizeof(a)/sizeof(int);
+  len /= sizeof(int);
   for (int i = 0;i<len;i++){
     sum += a[i];
   }
   return sum/len;
 }
 
-void clutchPOS(int pos) {
+int getSpeed() {
+  
+}
+
+void clutchPOS(int pos, int power) {
+  if (pos==CIN) {
+    btSerial.println("I");
+  }
+  else {
+    btSerial.println("O");
+  }
   while(analogRead(POS)-pos < -10 || analogRead(POS)-pos >10) {
     //safety to not move if goes to far, make higher than 5?
     if (analogRead(POS)>CIN+10) {
@@ -66,11 +95,11 @@ void clutchPOS(int pos) {
     }
     if (analogRead(POS) <pos){
       digitalWrite(DIR,HIGH);
-      analogWrite(PWM,255);
+      analogWrite(PWM,power);
     }
     if(analogRead(POS)>pos){
       digitalWrite(DIR,LOW);
-      analogWrite(PWM,100);
+      analogWrite(PWM,power/2);
     }
   }
   digitalWrite(PWM,LOW);
@@ -79,11 +108,11 @@ void clutchPOS(int pos) {
 
 void shiftDOWN() {
   //put clutch in 
-  clutchPOS(CIN);
+  btSerial.println("D");
+  clutchPOS(CIN, 255);
   //send serial message to shift down
   Serial.write('2');
-  //keep sending message every 10 milleseconds until it has any response back
-  //message can be anything
+
   int watchdog = 0;
   while (!Serial.available() and watchdog<80) {
     delay(10);  
@@ -91,17 +120,19 @@ void shiftDOWN() {
   }  
   if (!Serial.available()){
     failedResponseCounter++;
+    btSerial.println("F");
   }
   else{
-    //char a = Serial.read();
-    currentGear = Serial.read();//par(a);
+    currentGear = getGear();
+    btSerial.println(currentGear);
   }
-  clutchPOS(COUT);
+  clutchPOS(COUT, 255);
   //wait for message that shift succesful
 }
 
 void shiftUP() {
   Serial.write('1');
+  btSerial.println("U");
   int watchdog=0;
   while (!Serial.available() and watchdog <100) {
     delay(10);
@@ -109,30 +140,27 @@ void shiftUP() {
   }
   if (!Serial.available()) {
     failedResponseCounter++;
+    btSerial.println("F");
   }
   else {
-    //char a = Serial.read();
-    currentGear = Serial.read();//par(a); /////////////////
+    currentGear = getGear();
+    btSerial.println(currentGear);
   }
 }
 
 void neutral() {
-  clutchPOS(CIN);
+  clutchPOS(CIN, 255);
   Serial.write('5');
   int watchdog = 0;
   while (watchdog <100 and !Serial.available()){
     delay(10);
     watchdog++;
   }
-  if (!Serial.available()){
-    failedResponseCounter++;
-  }
-  else{
-    //char a = Serial.read();
-    currentGear = Serial.read();//par(a);
-  }
+  delay(500);
+  currentGear=getGear();
   if (currentGear==0){
-    clutchPOS(COUT);
+    clutchPOS(COUT, 255);
+    btSerial.println("N");
   }
 }
 
@@ -157,78 +185,75 @@ int par(char a) {
 }
 
 int getGear() {
+  int a=0;
+  while(Serial.available()) {
+    a=Serial.read();
+  }
   Serial.write('8');
-  /*while(!Serial.available()){
-    delay(10);
-  }*/
-  //char a = Serial.read();
-  return Serial.read();//par(a);
+  while(!Serial.available());
+  a = Serial.read();
+  return a;//par(a);
 }
 
 void launch() {
-  int pos = COUT;
-  while(analogRead(POS)-pos < -10 || analogRead(POS)-pos >10) {
-    //safety to not move if goes to far, make higher than 5?
-    if (analogRead(POS)>CIN+5) {
-      return;
-    }
-    if (analogRead(POS) <pos){
-      digitalWrite(DIR,LOW);
-      analogWrite(PWM,50);
-    }
-    if(analogRead(POS)>pos){
-      digitalWrite(DIR,HIGH);
-      analogWrite(PWM,50);
-    }
+  if (currentGear==0) {
+    clutchPOS(CIN, 255);
+    shiftUP();
   }
-  digitalWrite(PWM,LOW);
+  clutchPOS(COUT, 200);
+  isStopped=false;
 }
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, INPUT);
   pinMode(3, INPUT);
+  pinMode(RX, INPUT);
+  pinMode(TX, OUTPUT);
+  pinMode(BT_POWER, OUTPUT);
+  digitalWrite(BT_POWER, HIGH);
+  btSerial.begin(9600);
   Serial.begin(57600);
   attachInterrupt(digitalPinToInterrupt(2), interruptIRQ, RISING);
   attachInterrupt(digitalPinToInterrupt(3), NewinterruptIRQ, RISING);
   pinMode(POS,INPUT);
   pinMode(PWM,OUTPUT);
   pinMode(DIR,OUTPUT);
-  clutchPOS(COUT);
+  clutchPOS(COUT, 255);
+  delay(1000);
   currentGear = getGear();
+  clutchPOS(CIN,255);
+  delay(500);
+  clutchPOS(COUT, 255);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //print rpm values when update flage is true
-  
-  /*if (updateflag1) {
-    Serial.print("S1: ");
-    Serial.println(rpm);
-    updateflag1 = false;
-  }
-  if (updateflag2) {
-    Serial.print("S2: ");
-    Serial.println(rpm2);
-    updateflag2 = false;
-  }*/  
+   
   //clutchPOS(position)  moves clutch to position
-  if (rpm < changeRPM[currentGear-2])shiftDOWN();
-  if (rpm > 3700)shiftUP();
-  if (currentGear==1 and rpm<2000){
-    clutchPOS(CIN);
-    doNeutral = true;
+  if (currentGear>1) {
+    if (rpm2 < changeRPM[currentGear-2])shiftDOWN();
   }
-  if (doNeutral){
-    nwatchdog++; 
-    if (rpm<2000) doNeutral =  false; nwatchdog =0;
+  if (currentGear<5 and currentGear!=0) {
+    if (rpm2 > changeUpRPM[currentGear-1])shiftUP();
+  }
+  if (currentGear==1 and rpm2<2000){
+    clutchPOS(CIN, 255);
+    doNeutral=true;
+    isStopped=true;
+  }
+  else {
+    doNeutral=false;
+    nwatchdog=0;
+  }
+  if (doNeutral and currentGear!=0){
+    nwatchdog++;
     if (nwatchdog>80){
       neutral();
       nwatchdog = 0;
       doNeutral = false;
     }
   }
-  if (currentGear==1 and rpm>2400) {
+  if ((currentGear==1 or currentGear==0) and rpm2>2600 and isStopped) {
     launch();
   }
   if (failedResponseCounter > 2){
@@ -237,10 +262,35 @@ void loop() {
   }
   delay(50);
   wait_send_get_gear++;
-  
+  btSerial.println(rpm2);
   if (wait_send_get_gear >20) {
     currentGear = getGear();
     wait_send_get_gear = 0;
-  }
-  
+    btSerial.println(currentGear);
+  } 
 }
+
+//void loop() {
+//  delay(5000);
+//  char data[10];
+//  String dataString="rpm: " + String(rpm2);
+//  dataString.toCharArray(data, 10);
+//  Wire.beginTransmission(10);
+//  Wire.write(data);
+//  Wire.endTransmission();
+//  currentGear=getGear();
+//  dataString="gear: " + String(currentGear);
+//  dataString.toCharArray(data, 10);
+//  Wire.beginTransmission(10);
+//  Wire.write(data);
+//  Wire.endTransmission();
+//  if (currentGear==1) {
+//    Serial.write('5');
+//  }
+//  else if (currentGear==0) {
+//    Serial.write('1');
+//  }
+//  else {
+//    Serial.write('2');
+//  }
+//}
